580e59e4fdab0428d91935f2e43628479df1bcdf
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-08-13 19:08:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import getenv
+
+API_ID = int(getenv("API_ID", "14911221")) 
+API_HASH = getenv("API_HASH", "a5e14021456afd496e7377331e2e5bcf") 
+BOT_TOKEN = getenv("BOT_TOKEN", "6348327543:AAF4aaV1tSLaqibdiLc4hOviuNtsDH8vUsE")
+START_PIC = getenv("START_PIC", "https://telegra.ph//file/87fa102aa588b71f43b08.jpg")
